d26c5f34f8c02c5bde7ba0039d233f786a3074dd
Nikolay Tsonev <nikolay.tsonev@leanplum.com>
2020-07-23 16:45:35 +0300

    test sdk/webpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,8 @@ export class AppComponent implements OnInit {
    },
    showAds: true
   };
+  public variablesGet;
+  public inputVal ="test text web";
   constructor(){}
   ngOnInit(){
     if (this.isDevelopmentMode) {
@@ -36,29 +38,62 @@ export class AppComponent implements OnInit {
      Leanplum.default.start((success) => {
       console.log('Success: ' + success);
       console.log('Variables', Leanplum.default.getVariables());
-      Leanplum.default.setUserId("webUser");
-      
+      Leanplum.default.setUserId("2webUser");
+      this.variablesGet = Leanplum.default.getVariables();
+      console.log("variables",this.variablesGet);
+      Leanplum.default.on('showMessage', function (args){
+        var message = args.message;
+        var context = args.context;
+        console.log("MSG", message)
+        // 2. Filter out unsupported message types
+        if (message.__name__ !== 'Confirm') {
+          return;
+        }
+        
+        // 3. Track impression
+        context.track();
+        
+        // 4. Show message and trigger attached actions
+        //    (message can be rendered in HTML and actions can be called asynchronously)
+        if (confirm(message.Message)) {
+          context.runTrackedActionNamed('Accept action');
+        } else {
+          context.runTrackedActionNamed('Cancel action');
+        }
+      });
       if(Leanplum.default.isWebPushSupported()){
         Leanplum.default.isWebPushSubscribed().then(wpr=>{
           console.log("WPR", wpr);
           if(!wpr){
             console.log("Leanplum register for push");
-            Leanplum.default.registerForWebPush("../assets/sw.js").then(r=>{
+            Leanplum.default.registerForWebPush("assets/sw.js").then(r=>{
               console.log("Register push result", r);
+              
             }).catch(e=>{
               console.log("Error push result", e);
             })
           } else{
-            Leanplum.default.unregisterFromWebPush().then(r=>{
-              console.log("Unregister push result", r);
-            }).catch(e=>{
-              console.log("Error push unregister", e);
-            })
+            // Leanplum.default.unregisterFromWebPush().then(r=>{
+            //   console.log("Unregister push result", r);
+            // }).catch(e=>{
+            //   console.log("Error push unregister", e);
+            // })
+            
           }
         }).catch(wpe=>{
           console.log("err", wpe)
         })
       }
      });
+  }
+  setVar(){
+    console.log("CLICK - val", this.inputVal);
+    Leanplum.default.setVariables({"myWebVar":this.inputVal});
+    setTimeout(() => {
+      this.variablesGet = Leanplum.default.getVariables();
+    }, 2000);
+  }
+  trackE(){
+    Leanplum.default.track("Web like", {"myWebVarInput":this.inputVal});
   }
 }
